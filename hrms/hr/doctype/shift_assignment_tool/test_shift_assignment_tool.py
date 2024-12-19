58d37c6213654b7fa105c720e783dd9b3ff79544
krantheman <akash.akasht.tom@gmail.com>
2024-12-19 16:48:02 +0530

    fix: tests

--- hrms/hr/doctype/shift_assignment_tool/test_shift_assignment_tool.py
+++ hrms/hr/doctype/shift_assignment_tool/test_shift_assignment_tool.py
@@ -2,7 +2,7 @@
 # See license.txt
 
 import frappe
-from frappe.tests import IntegrationTestCase, change_settings
+from frappe.tests.utils import FrappeTestCase, change_settings
 from frappe.utils import add_days, getdate
 
 from erpnext.setup.doctype.employee.test_employee import make_employee
@@ -14,7 +14,7 @@ from hrms.hr.doctype.shift_type.test_shift_type import make_shift_assignment, se
 from hrms.tests.test_utils import create_company
 
 
-class TestShiftAssignmentTool(IntegrationTestCase):
+class TestShiftAssignmentTool(FrappeTestCase):
 	def setUp(self):
 		create_company()
 		create_company("_Test Company2")
