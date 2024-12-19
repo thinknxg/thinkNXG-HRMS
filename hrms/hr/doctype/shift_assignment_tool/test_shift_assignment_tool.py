a98ad0308d1bcbedc6f2af87c48402b1bbe00339
krantheman <akash.akasht.tom@gmail.com>
2024-12-19 15:57:32 +0530

    fix: patch
    (cherry picked from commit cfb5c8521af3b5dece8d1bd43ac584dcf34d40f1)

--- hrms/hr/doctype/shift_assignment_tool/test_shift_assignment_tool.py
+++ hrms/hr/doctype/shift_assignment_tool/test_shift_assignment_tool.py
@@ -235,13 +235,12 @@ class TestShiftAssignmentTool(FrappeTestCase):
 		shift_assignment_tool = ShiftAssignmentTool(args)
 
 		employees = [self.emp1, self.emp2, self.emp3]
-		shift_assignment_tool.bulk_assign(employees)
+		shift_assignment_tool._bulk_assign(employees)
 		assigned_employees = frappe.get_list(
 			"Shift Schedule Assignment",
 			filters={
 				"shift_schedule": self.schedule1,
 				"shift_status": "Active",
-				"create_shifts_after": today,
 				"enabled": 0,
 			},
 			pluck="employee",
