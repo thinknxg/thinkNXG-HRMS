dc83204372b371b72f2c48db5ff4a93bfeeee7f9
Rijin Raju <rijinraju856@gmail.com>
2025-01-24 18:07:32 +0530

    Updated: logos and title

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -1,17 +1,17 @@
 app_name = "hrms"
-app_title = "Frappe HR"
-app_publisher = "Frappe Technologies Pvt. Ltd."
+app_title = "thinkNXG HR"
+app_publisher = "thinkNXG Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
 required_apps = ["frappe/erpnext"]
-source_link = "http://github.com/frappe/hrms"
+source_link = "https://github.com/thinknxg/thinknxg-hrms.git"
 
 add_to_apps_screen = [
 	{
-		"name": "hrms",
-		"logo": "/assets/hrms/images/frappe-hr-logo.svg",
-		"title": "Frappe HR",
+		"name": "thinkNXG HR",
+		"logo": "/assets/hrms/images/thinkNXG-logo.jpg",
+		"title": "thinkNXG HR",
 		"route": "/app/hr",
 		"has_permission": "hrms.hr.utils.check_app_permission",
 	}
