91ea067449133911630e5ad417f0243f1a641022
Aysha <74527579+AyshaHakeem@users.noreply.github.com>
2025-02-10 16:34:39 +0530

    feat: payment entry for leave encashment

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -241,7 +241,7 @@ scheduler_events = {
 	"monthly": ["hrms.controllers.employee_reminders.send_reminders_in_advance_monthly"],
 }
 
-advance_payment_doctypes = ["Gratuity", "Employee Advance"]
+advance_payment_payable_doctypes = ["Leave Encashment", "Gratuity", "Employee Advance"]
 
 invoice_doctypes = ["Expense Claim"]
 
