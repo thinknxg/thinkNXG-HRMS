04af52aa13cbb880c4c1dc6242468bb62e60a5a9
RijinRaju <rijinraju856@gmail.com>
2025-02-10 15:30:04 +0530

    Fix: updated titles and logos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,8 +23,8 @@ export default defineConfig({
 			},
 			manifest: {
 				display: "standalone",
-				name: "Frappe HR",
-				short_name: "Frappe HR",
+				name: "thinkNXG HR",
+				short_name: "thinkNXG HR",
 				start_url: "/hrms",
 				description: "Everyday HR & Payroll operations at your fingertips",
 				theme_color: "#ffffff",
